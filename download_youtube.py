a8caddc666bf437751422f0d1578f2f155d48bf5
Peash Das Rudra <78162356+peashdasrudra@users.noreply.github.com>
2024-03-05 00:05:28 +0600

    Add files via upload

--- download_youtube.py
+++ download_youtube.py
@@ -1,23 +1,25 @@
 from pytube import YouTube
 import sys
 
-# Check if correct number of arguments are provided
 if len(sys.argv) != 2:
-    print("Usage: python script_name.py <YouTube Video URL>")
+    
+    print("Please Enter The Youtube Video Link -> Usage: py download_youtube.py <YouTube Video URL>")
     sys.exit(1)
 
-# Fetch the YouTube link from command line argument
+
 link = sys.argv[1]
 
-# Download video
+
 try:
     yt = YouTube(link)
     print("Title: ", yt.title)
     print("Views: ", yt.views)
     yd = yt.streams.get_highest_resolution()
-    # Corrected path with proper backslash escaping
+
+    # Ensure The Corrected path with proper backslash escaping
     yd.download('Downloads')
-    print("Download completed successfully.")
+    print("Download completed successfully!")
+
 except Exception as e:
     print("Error:", e)
 
